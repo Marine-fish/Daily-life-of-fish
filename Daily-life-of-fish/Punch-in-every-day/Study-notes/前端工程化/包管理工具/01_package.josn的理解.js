94de3d9a6c29087d034ed7da2e702d27514e6351
pzq10237991 <1798919527@qq.com>
2023-06-13 00:04:48 +0800

    node

---
+++ Daily-life-of-fish/Punch-in-every-day/Study-notes/前端工程化/包管理工具/01_package.josn的理解.js
@@ -0,0 +1,17 @@
+/***
+ * name - 项目的名字
+ * version - 版本号
+ * dependencies - 依赖的东西
+ * 用来记录项目的配置信息
+ * ....
+ * 创建package.json文件的方法:
+ * npm init
+ * 执行流程
+ * package name 取什么名字
+ * version 版本号
+ * description 描述信息
+ * entry point 配置文件路径入口
+ * 
+ * npm init -y
+ * 自动填写package.json文件信息
+ */
